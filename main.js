b719fa276200a3aae7e7e67786426b4fd720a4e3
Raj <rajrv559910@gmail.com>
2020-07-30 17:01:29 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', ()=>{
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    openMenu.style.display = 'none';
+});
+
+closeMenu.addEventListener('click', ()=>{
+    mainMenu.style.top = '-100%';
+    openMenu.style.display = 'block';
+});
